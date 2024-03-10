71701f7b65f1c20b6f1416847819b47709693939
Alisson Youssf <youssfbr@gmail.com>
2024-03-10 17:49:37 -0300

    Initial commit

---
+++ src/main/java/com/github/youssfbr/uri2611/Uri2611Application.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.uri2611;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uri2611Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2611Application.class, args);
+	}
+
+}
